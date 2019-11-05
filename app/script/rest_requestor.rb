174a17a09ce8ecbebd27a03068534f49ec3c58be
thsvr <vr.ths.zd@gmail.com>
2019-11-05 14:32:36 -0300

    add url variables to edit, new, show

--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -2,4 +2,10 @@
 require 'rest-client'
 url = "http://localhost:3000/users"
 
+url_new = "http://localhost:3000/users/new"
+
+url_edit = "http://localhost:3000/users/edit"
+
+url_show = "http://localhost:3000/users/show"
+
 puts RestClient.get(url)
